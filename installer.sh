5ad0cf44a1ab1457e3624bf0d8985320e8e05709
Soleil-des-chats <61901116+Soleil-des-chats@users.noreply.github.com>
2023-01-24 21:59:28 +0100

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,8 @@
+sudo apt install python3-dbus libhidapi-hidraw0
+sudo pip3 install -e .
+sudo apt install git
+git clone https://github.com/NswitcheR-Server/c.git
+cd Nswitcher-Server
+sudo python3 switchremoteplay/server.py --help
+echo This command to launch : sudo SECRET_KEY='Valeur Aléatoire ici' python3 switchremoteplay/server.py
+echo -r <Switch MAC address> to reconnect to an already paired Switch.
