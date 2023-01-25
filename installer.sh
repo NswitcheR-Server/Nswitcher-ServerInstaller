aca0bb97ee53c8deb43d632c26a8e70e381df68b
Soleil-des-chats <61901116+Soleil-des-chats@users.noreply.github.com>
2023-01-25 13:47:59 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 sudo apt install python3-dbus libhidapi-hidraw0
 sudo pip3 install -e .
 sudo apt install git
-git clone https://github.com/NswitcheR-Server/c.git
+git clone https://github.com/NswitcheR-Server/Nswitcher-Server.git
 cd Nswitcher-Server
 sudo python3 switchremoteplay/server.py --help
 echo This command to launch : sudo SECRET_KEY='Valeur Aléatoire ici' python3 switchremoteplay/server.py
